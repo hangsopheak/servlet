db4860b64545ae4d29d99a32d4ee10138eff2e66
Sophea Mak <sm@goldengekko.com>
2017-01-03 22:13:49 +0700

    add session object

---
+++ src/main/java/com/rupp/sample/web/ResponseError.java
@@ -0,0 +1,17 @@
+package com.rupp.sample.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ResponseError  extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Set error code and reason.
+        resp.sendError(407, "Need authentication!!!" );
+    }
+
+}
